83b40c5349adeeb5be51cb2272e968b2679a0f2c
Heenabaig <44297466+Heenabaig@users.noreply.github.com>
2020-07-15 12:53:36 +0530

    Add files via upload

---
+++ firstscript.js
@@ -0,0 +1,7 @@
+//alert('hey click');
+console.log('Hello Developer');
+console.warn('Dont click');
+console.time('Current time is :');
+console.count('counting');
+console.table({'apple':1, 'kiwi':3});
+
